15e2cc7557d9245735716a2021b380ddbff5c91e
veeraphatnick <58050379@kmitl.ac.th>
2019-04-09 16:07:21 +0700

    09/04/19

--- Dash-User-Guide-and-Documentation/Component-Libraries/Dash-Core-Components-Libraries/13_Dash_Upload_Component.py
+++ Dash-User-Guide-and-Documentation/Component-Libraries/Dash-Core-Components-Libraries/13_Dash_Upload_Component.py
@@ -1,26 +1,26 @@
+import base64
+import datetime
+import io
+
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
+import dash_table
 
-import base64
-import json
 import pandas as pd
-import plotly
-import io
 
-app = dash.Dash()
 
-app.scripts.config.serve_locally = True
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.Div(id='waitfor'),
     dcc.Upload(
-        id='upload',
+        id='upload-data',
         children=html.Div([
             'Drag and Drop or ',
-            html.A('Select a File')
+            html.A('Select Files')
         ]),
         style={
             'width': '100%',
@@ -31,60 +31,62 @@ app.layout = html.Div([
             'borderRadius': '5px',
             'textAlign': 'center',
             'margin': '10px'
-        }
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
     ),
-    html.Div(id='output'),
-    html.Div(dt.DataTable(rows=[{}]), style={'display': 'none'})
+    html.Div(id='output-data-upload'),
 ])
 
-pre_style = {
-    'whiteSpace': 'pre-wrap',
-    'wordBreak': 'break-all',
-    'whiteSpace': 'normal'
-}
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df.to_dict('rows'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
 
 
-@app.callback(Output('output', 'children'),
-              [Input('upload', 'contents')])
-def update_output(contents):
-    if contents is not None:
-        content_type, content_string = contents.split(',')
-        if 'csv' in content_type:
-            df = pd.read_csv(io.StringIO(base64.b64decode(content_string).decode('utf-8')))
-            return html.Div([
-                dt.DataTable(rows=df.to_dict('records')),
-                html.Hr(),
-                html.Div('Raw Content'),
-                html.Pre(contents, style=pre_style)
-            ])
-        elif 'image' in content_type:
-            return html.Div([
-                html.Img(src=contents),
-                html.Hr(),
-                html.Div('Raw Content'),
-                html.Pre(contents, style=pre_style)
-            ])
-        else:
-            # xlsx will have 'spreadsheet' in `content_type` but `xls` won't
-            # have anything
-            try:
-                df = pd.read_excel(io.BytesIO(base64.b64decode(content_string)))
-                return html.Div([
-                    dt.DataTable(rows=df.to_dict('records')),
-                    html.Hr(),
-                    html.Div('Raw Content'),
-                    html.Pre(contents, style=pre_style)
-                ])
-            except:
-                return html.Div([
-                    html.Hr(),
-                    html.Div('Raw Content'),
-                    html.Pre(contents, style=pre_style)
-                ])
-
-app.css.append_css({
-    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
-})
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 if __name__ == '__main__':
     app.run_server(debug=True)
